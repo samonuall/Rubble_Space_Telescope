301e2473b3c2482aead289c23f4233e03491e403
hgandor <hgandor@mit.edu>
2020-07-27 00:47:12 +0100

    updated bluetooth functions to return telemetry data

--- bluetooth/RSTbluetooth.py
+++ bluetooth/RSTbluetooth.py
@@ -1,31 +1,60 @@
 import subprocess
+import time
+import os
+
+def getSizeKB(filepath):
+    return(os.path.getsize(filepath)/1024.0)
 
 def sendFile(bdaddr, filepath):
     # Attempts to send a file over obexftp and returns if it was successful or not
     #
     # param:
     #   string bdaddr: the bluetooth device address to send to (must be connected and have file transfer set up to work)
-    #   string filepath: the path of the file that will be transmitted
+    #   string filepath: the path of the file that will be transmitted to the device
     #
     # return:
-    #   True: the file was sent successfully over obexftp
-    #   False: the file was not sent successfully
+    #   boolean successfulTransfer: True or False value representing whether or not the file was successfully sent
+    #   float fileSize: the size of the file being sent in kB
+    #   int secondsToSend: the time in seconds that it took to send the file
+    preTransfer = time.time()
     transferProcess = subprocess.getoutput("obexftp -b {} -c / -p {}".format(bdaddr, filepath))
+    postTransfer = time.time()
     transferProcess = transferProcess.replace("..."," ").replace(":"," ").replace("."," ").replace("\n"," ").split(" ")
-    return(not "failed" in transferProcess)
 
-def getFile(bdaddr, filepath):
+    successfulTransfer = not "failed" in transferProcess
+    fileSize = getSizeKB(filepath)
+    secondsToSend = postTransfer - preTransfer
+    if(successfulTransfer):
+        return successfulTransfer, fileSize, secondsToSend
+    else:
+        return(successfulTransfer, None, None)
+
+def getFile(bdaddr, filepath, toDir=None):
     # Attempts to download a file over obexftp and returns if it was successful or not
-    # The file is downloaded to the directory of the python script it is run from
+    # The file is downloaded to the directory the script was run from, and if specified, is moved to a different directory
     #
     # param:
     #   string bdaddr: the bluetooth device address to download from (must be connected and have file transfer set up to work)
-    #   string filepath: the path of the file that will be downloaded
+    #   string filepath: the path of the file that will be downloaded from the device
+    #   string toDir: the path to the directory the file should be moved to (ex. "path/to/directory/")
     #
     # return:
-    #   True: the file was downloaded successfully over obexftp
-    #   False: the file was not downloaded successfully
+    #   boolean successfulRetrieval: True or False value representing whether or not the file was retrieved successfully
+    #   float fileSize: the size of the file retrieved in kB
+    #   int secondsToGet: the time in seconds that it took to retrieve the file
+    preRetrieve = time.time()
     retrieveProcess = subprocess.getoutput("obexftp -b {} -g {}".format(bdaddr, filepath))
-    retrieveProcess = retrieveProcess.replace("..."," ").replace(":"," ").replace("."," ").replace("\n"," ").split(" ")
-    return(not "failed" in retrieveProcess)
+    postRetrieve = time.time()
 
+    secondsToGet = None
+    fileSize = None
+    filename = filepath.split("/")[-1]
+    fileExists = os.path.isfile(filename)
+
+    if(fileExists):
+        if(toDir!=None):
+            toDir = toDir + filename
+            os.rename(filename, toDir)
+        secondsToGet = postRetrieve - preRetrieve
+        fileSize = getSizeKB(toDir)
+    return(fileExists, fileSize, secondsToGet)
