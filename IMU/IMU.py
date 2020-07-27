01ef40d2e76c2df388e07b1ef1f9228e9dc6446e
sajiv <sajiv@mit.edu>
2020-07-27 07:10:43 -0700

    added time functions and yaw logic

--- IMU/IMU.py
+++ IMU/IMU.py
@@ -4,41 +4,73 @@ import board
 import busio
 import adafruit_fxos8700
 import adafruit_fxas21002c
-import numpy
+import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_fxos8700.FXOS8700(i2c)   
+sensor = adafruit_fxos8700.FXOS8700(i2c)
+
+#all variables
+yaw = 0
+imgcounter = 0
+time = 0
+orbitCount = 1
+telemString = ""
 
 while True:
     accel_x, accel_y, accel_z = sensor.accelerometer
     mag_x, mag_y, mag_z = sensor.magnetometer
 
+    roll = np.arctan2((accelY),(((accelX)**2 + (accelZ)**2)**0.5))
+    pitch = np.arctan2((accelX),(((accelY)**2 + (accelZ)**2)**0.5))
+
+    mag_x = (magX * np.cos(pitch)) + (magY * np.sin(roll)*np.sin(pitch)) + (magZ*np.cos(roll)*np.sin(pitch))
+    mag_y = (magY*np.cos(roll)) - (magZ*np.sin(roll))
+    #Original Yaw Eq: yaw = (180/np.pi)*np.arctan2(-mag_y, mag_x)
+    yaw = (180/np.pi)*np.arctan2(-mag_y, mag_x)
+    #Fixing
+    yaw = (yaw + 360) % 360
+    pitch = (((180/np.pi) * pitch) + 360) % 360
+    roll = (((180/np.pi) * roll) + 360) % 360
+
+    #yaw to 0-360
+    if yaw > 360:
+        yaw -= 360
+    #set orbit end
+    if(imgcounter >= 3):
+        endOrbit()
+        imgcounter = 0
 '''
 the master master code needs to call functions from the IMU fuile: we determined 3 functions
 1) run a test code to make sure the IMU is working
 2) boot up the IMu and begin reporting readings
 3) return what orbit the satteltile is on (orbit number 1, etc.)
 '''
-yaw = 0
-orbitCount = 0
-telemString = ""
-def boot():
-    
+
+def starttest():
+    time()
+    return yaw
 def orbitType():
     if(orbitCount == 1 or orbtiCount == 4 or completedOrbits == 7):
         return "Image"
     else:
-        return "Tranfer"  
-        
+        return "Tranfer"
+
 def overImage():
     if((yaw in range(350,360)) or (yaw in range (0,10)) or (yaw in range (110,130)) or (yaw in range (230,250)):
+        imgcounter += 1
+        telemString += " Image taken at " + time
         return True
     else:
       overImage()
 
 def endOrbit():
-    orbitCount++
-    telemString += orbitCount
+    orbitCount += 1
+    telemString += " Starting orbit " + orbitCount
     return telemString
+
+def time():
+    while True:
+        time.sleep(0.1)
+        time += 0.1
