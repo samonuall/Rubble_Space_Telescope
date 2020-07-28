d9964b71a98d57ca213bc47d2ca30fb7a026d280
SajivShah <sajiv@mit.edu>
2020-07-28 16:03:15 -0700

    fixed file directory in camera_capture

--- camera_capture.py
+++ camera_capture.py
@@ -2,9 +2,9 @@ from picamera import PiCamera
 import time
 
 def take_picture(orbit_number):
-	with PiCamera() as camera:
-		timestamp = time.strftime('%M:%s')[:-8]
-		name = 'data_transfer/{}_{}.jpg'.format(orbit_number, timestamp)
-		camera.capture(name, quality=100)
+    with PiCamera() as camera:
+        timestamp = time.strftime('%M:%s')[:-8]
+        name = '/home/pi/Rubble_Space_Telescope/data_transfer/{}_{}.jpg'.format(orbit_number, timestamp)
+        camera.capture(name, quality=100)
 
-	return name
+    return name
