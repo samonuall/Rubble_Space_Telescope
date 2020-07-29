715f05aade2bbe340e5217945068db523bec9833
sonuall <samonuall@gmail.com>
2020-07-28 21:12:43 -0400

    Change naming convention

--- camera_capture.py
+++ camera_capture.py
@@ -3,7 +3,7 @@ import time
 
 def take_picture(orbit_number):
     with PiCamera() as camera:
-        timestamp = time.strftime('%M:%s')[:-8]
+        timestamp = time.strftime('%S')
         name = '/home/pi/Rubble_Space_Telescope/data_transfer/{}_{}.jpg'.format(orbit_number, timestamp)
         camera.capture(name, quality=75)
 
